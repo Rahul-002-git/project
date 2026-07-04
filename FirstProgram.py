54f5216b9a854774170cfa05ff56dddb59d90bbb
Rahul-002-git <thrahul90@gmail.com>
2026-07-04 10:10:11 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,27 @@
+# a= 2
+# b= 3
+# sum= a+b
+# # print(sum)
+# # print(a*b)
+# # arithmetic operators
+# a=7
+# b=8
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# a=50
+# b=40
+# print(a==b)
+# print(a!=b)
+#num=20
+# num=num+200
+# print(num)
+# 
+a=int("2")
+b=8
+print(a+b)
+name=input("enter your name")
+print("welcome,name")
